328ce9f89ea3dd450d6809f07a09f94bb969ad60
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2024-03-04 22:30:00 +0530

    Add files via upload

---
+++ 26_Program to Calculate total number of possible handhsake for given n people.py
@@ -0,0 +1,8 @@
+#Python Program to calculate maximum number of handshakes
+
+
+n = int(input("Enter the number of people "))
+
+Total_number = int(n * ((n-1)/2))
+
+print("The total number of Hand shakes is :- ",Total_number, "handshakes")
