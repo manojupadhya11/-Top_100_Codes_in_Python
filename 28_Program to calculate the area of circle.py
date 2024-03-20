09a7dcb7c03de49d8799782889eb3036d39e6c7b
Manoj Upadhya <70696140+manojupadhya11@users.noreply.github.com>
2024-03-20 16:24:58 +0530

    Add files via upload

---
+++ 28_Program to calculate the area of circle.py
@@ -0,0 +1,7 @@
+#Python Program to calculate the area of circle
+import math
+
+radius  =  int(input("Enter the Radius of the Circle  "))
+area = math.pi*radius*radius
+
+print("The Area of the Circle is :- ", area)
---
+++ 29_Program to calculate the area od square.py
@@ -0,0 +1,6 @@
+#Python Program to calculate the area of Square
+import math
+
+length  =  int(input("Enter the length in "))
+area = length*length
+print("The Area of the Square is :- ", area)
